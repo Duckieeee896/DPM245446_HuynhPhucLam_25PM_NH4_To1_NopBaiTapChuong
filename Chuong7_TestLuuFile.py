72a9603fd9252e31a8318f483f143a0ea9516a3a
Duckieeee896 <lamericlx2006@gmail.com>
2025-12-02 12:03:31 +0700

    Nộp bài tập Python

--- Chuong7_TestLuuFile.py
+++
@@ -1,7 +0,0 @@
-from Chuong7_XuLyFile import * 
-masp=input("nhập mã SP:") 
-tensp=input("nhập tên sp:") 
-dongia=float(input("nhập giá:")) 
-line=masp+";"+tensp+";"+str(dongia) 
- 
-LuuFile("database.txt",line)
